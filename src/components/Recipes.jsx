72eb2869983d00e535d9288dc46dbcb2c22e1b07
christianrafols17 <christianrafols367@gmail.com>
2023-10-20 23:51:37 +0800

    Added Sort by Country Function

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -5,15 +5,20 @@ import Modal from './Modal/Modal';
 
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
+
   const [searchRecipe, setSearchRecipe] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+
+  const [searchArea, setSearchArea] = useState('');
+  const [searchAreaResults, setSearchAreaResults] = useState([]);
+
   const API_KEY = '1';
 
   const [modal, setModal] = useState(false);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
 
   useEffect(() => {
-    const apiURL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+    const apiURL = `https://www.themealdb.com/api/json/v1/${API_KEY}/search.php?s=`;
     axios.get(apiURL)
     .then((response) => {
       setRecipes(response.data.meals);
@@ -24,30 +29,42 @@ const Recipes = () => {
   }, []);
 
   useEffect(() => {
-      const fetchMeals = async () => {
-        try {
-          const response = await fetch(
-            `https://www.themealdb.com/api/json/v1/${API_KEY}/search.php?s=${searchRecipe}`
-          );
-  
-          if (response.ok) {
-            const data = await response.json(); 
-            setSearchResults(data.meals);
-          } else {
-            setSearchResults([]);
-          }
-        } catch (error) {
-          console.error('Error fetching data:', error);
+    const apiAreaURL = `https://www.themealdb.com/api/json/v1/${API_KEY}/filter.php?a=${searchArea}`;
+    axios.get(apiAreaURL)
+    .then((response) => {
+      console.log('API Response for searchArea:', response.data);
+      setSearchAreaResults(response.data.meals);
+    })
+    .catch((error) => {
+      console.error('Error fetching data: ', error);
+    })
+  }, [searchArea]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch(
+          `https://www.themealdb.com/api/json/v1/${API_KEY}/search.php?s=${searchRecipe}`
+        );
+
+        if (response.ok) {
+          const data = await response.json(); 
+          setSearchResults(data.meals);
+        } else {
           setSearchResults([]);
         }
-      };
-  
-      if (searchRecipe) {
-        fetchMeals();
-        setRecipes([]);
-      } else {
+      } catch (error) {
+        console.error('Error fetching data:', error);
         setSearchResults([]);
       }
+    };
+
+    if (searchRecipe) {
+      fetchMeals();
+      setRecipes([]);
+    } else {
+      setSearchResults([]);
+    }
   }, [searchRecipe]); 
 
   const allIngredients = (meal) => {
@@ -65,8 +82,12 @@ const Recipes = () => {
   }
 
   const handleInputChange = (event) => {
-      setSearchRecipe(event.target.value);
+    setSearchRecipe(event.target.value);
   };
+
+  const handleDropdownChange = (event) => {
+    setSearchArea(event.target.value);
+  }
 
   const openModal = (meal) => {
     const ingredients = allIngredients(meal);
@@ -81,10 +102,22 @@ const Recipes = () => {
     setModal(false);
   };   
 
+  console.log(searchArea)
+  console.log(searchAreaResults)
+
   return (
   <div name="Recipes" className='w-full min-h-screen bg-gradient-to-b from-gray-200 to-indigo-200 px-4 md:px-24 pt-16 md:pt-24'>
       <div className='flex flex-row py-4 items-center'>
           <h1 className='text-2xl md:text-4xl font-bold'>Recipes</h1>
+          <div className='ms-auto'>
+            <label className='text-lg'>Sort By Country: </label>
+            <select id='areaSelect' name='areaSelect' value={searchArea} onChange={handleDropdownChange} className='text-gray-400 p-2 border border-yellow-500 rounded-md'>
+              <option value=''>Select Country</option>
+              <option value='Canadian'>Canada</option>
+              <option value='Chinese'>China</option>
+              <option value='Filipino'>Philippines</option>
+            </select>
+          </div>
           <span className=' border border-yellow-400 rounded-lg w-2/4 md:w-1/3 ms-auto p-2 flex flex-row items-center'>
               <input type='text' placeholder='Search Recipe' value={searchRecipe} onChange={handleInputChange} className='bg-transparent w-full focus:outline-none'/>
               <input type='submit' value='' />  <BsSearch size={18} className='hover:cursor-pointer ms-auto'/>
@@ -101,6 +134,17 @@ const Recipes = () => {
             <span className='text-sm'>{meal.strCategory}</span>
             <button className='bg-white p-2 rounded-md' onClick={() => openModal(meal)}>View Recipe</button>
           </div>
+        ))}
+
+        {searchAreaResults && 
+          searchAreaResults.length > 0 &&
+          searchAreaResults.map((meal) => (
+            <div key={meal.idMeal} className='duplicate bg-white bg-opacity-20 rounded-md flex flex-col h-[240px] justify-between text-center shadow-md'>
+              <img className='w-full h-1/2 object-cover rounded-t-md hover:scale-105 duration-200' src={meal.strMealThumb} alt={meal.strMeal}/>
+              <h1 className='text-base font-semibold'>{meal.strMeal}</h1>
+              <span className='text-sm'>{meal.strCategory}</span>
+              <button className='bg-white p-2 rounded-md' onClick={() => openModal(meal)}>View Recipe</button>
+            </div>
         ))}
 
         {searchResults &&
